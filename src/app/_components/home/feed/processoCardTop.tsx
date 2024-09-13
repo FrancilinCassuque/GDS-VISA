19ffd98ec6b00811325908a8621e53a72f283d3e
Mateus Cassuque <mateusabril7@gmail.com>
2024-09-13 16:32:10 +0100

    Tushe!

--- src/app/_components/home/feed/processoCardTop.tsx
+++ src/app/_components/home/feed/processoCardTop.tsx
@@ -2,18 +2,18 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Card, CardHeader, CardTitle, CardDescription, CardFooter, CardContent } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
-import { IClient } from "@/types"
-import { IconUserPlus } from "../.."
+import { Newspaper } from "lucide-react"
+import { IProcesso } from "@/types"
 
 interface ICardProps {
-  clientes: IClient[]
+  processos: IProcesso[]
 }
 
-export const CardClientsHomeTop: React.FC<ICardProps> = async ({ clientes }) => {
+export const CardProcessoHomeTop: React.FC<ICardProps> = async ({ processos }) => {
 
-  if (clientes instanceof Error) return
+  if (processos instanceof Error) return
 
-  const semanaCliActual = clientes.filter(cli => {
+  const semanaCliActual = processos.filter(cli => {
     if ((cli.updatedAt.getFullYear() == new Date().getFullYear()) &&
       (cli.updatedAt.getMonth() == new Date().getMonth()) &&
       (cli.updatedAt.getDate() >= new Date().getDate() - 7) &&
@@ -22,7 +22,7 @@ export const CardClientsHomeTop: React.FC<ICardProps> = async ({ clientes }) => 
     }
   })
 
-  const semanaCliPassada = clientes.filter(cli => {
+  const semanaCliPassada = processos.filter(cli => {
     if ((cli.updatedAt.getFullYear() == new Date().getFullYear()) &&
       (cli.updatedAt.getMonth() == new Date().getMonth()) &&
       (cli.updatedAt.getDate() >= new Date().getDate() - 14) &&
@@ -37,27 +37,27 @@ export const CardClientsHomeTop: React.FC<ICardProps> = async ({ clientes }) => 
     <>
       <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
         <CardHeader className="pb-3">
-          <CardTitle>Clientes</CardTitle>
+          <CardTitle>Processos</CardTitle>
           <CardDescription className="max-w-lg text-balance leading-relaxed">
-            Registra o Cliente para poder Abrir um Processo.
+            Abrir um Processo.
           </CardDescription>
         </CardHeader>
         <CardFooter>
-          <Link href={'/auth/client/create'}>
+          <Link href={'/auth/processo/create'}>
             <Button>
-              <IconUserPlus /> <span className="px-4">Registrar novo </span>
+              <Newspaper /> <span className="px-4">Novo Processo </span>
             </Button>
           </Link>
         </CardFooter>
       </Card>
 
       {
-        clientes.length > 0 && (
+        processos.length > 0 && (
           <>
             <Card x-chunk="dashboard-05-chunk-1">
               <CardHeader className="pb-2">
-                <CardDescription>Total de Clientes</CardDescription>
-                <CardTitle className="text-4xl text-center">{clientes.length}</CardTitle>
+                <CardDescription>Total de processos</CardDescription>
+                <CardTitle className="text-4xl text-center">{processos.length}</CardTitle>
               </CardHeader>
               <CardContent>
                 <div className="text-xs text-muted-foreground">{`+${EssaSemanaPorcento}% Essa semana`}</div>
--- src/app/_components/home/homeDashboard.tsx
+++ src/app/_components/home/homeDashboard.tsx
@@ -5,6 +5,7 @@ import { Progress } from "@/components/ui/progress"
 import { IconUserPlus, TabDashboard } from ".."
 import { Newspaper } from "lucide-react"
 import { ClientIndex } from "@/db"
+import { CardClientsHomeTop } from "./feed/clientCardTop"
 
 export const HomeDashboard: React.FC = async () => {
   const clientes = await ClientIndex()
@@ -35,48 +36,7 @@ export const HomeDashboard: React.FC = async () => {
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3 sm:mb-64">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-          <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
-            <CardHeader className="pb-3">
-              <CardTitle>Clientes</CardTitle>
-              <CardDescription className="max-w-lg text-balance leading-relaxed">
-                Registra o Cliente para poder Abrir um Processo.
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Link href={'/auth/client/create'}>
-                <Button>
-                  <IconUserPlus /> <span className="px-4">Registrar novo </span>
-                </Button>
-              </Link>
-            </CardFooter>
-          </Card>
-
-          <Card x-chunk="dashboard-05-chunk-1">
-            <CardHeader className="pb-2">
-              <CardDescription>Total de Clientes</CardDescription>
-              <CardTitle className="text-4xl text-center">{clientes.length}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-xs text-muted-foreground">{`+${EssaSemanaPorcento}% Essa semana`}</div>
-            </CardContent>
-            <CardFooter>
-              <Progress value={EssaSemanaPorcento} aria-label="25% increase" />
-            </CardFooter>
-          </Card>
-
-          {/* <Card x-chunk="dashboard-05-chunk-2">
-            <CardHeader className="pb-2">
-              <CardDescription>This Month</CardDescription>
-              <CardTitle className="text-4xl">$5,329</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-xs text-muted-foreground">+10% from last month</div>
-            </CardContent>
-            <CardFooter>
-              <Progress value={12} aria-label="12% increase" />
-            </CardFooter>
-          </Card> */}
-
+          <CardClientsHomeTop clientes={clientes} />
         </div>
         
         <TabDashboard clientes={clientes}/>
