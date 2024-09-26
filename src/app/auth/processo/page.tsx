42e36a4de961cf2d9684279065d643ab0551089b
Mateus Cassuque <mateusabril7@gmail.com>
2024-09-26 12:07:14 +0100

    Perfil
    Deploy Rigo!

--- src/app/auth/processo/page.tsx
+++ src/app/auth/processo/page.tsx
@@ -14,20 +14,20 @@ export default async function Page() {
   return (
     <>
       <Breadcrumb className="hidden md:flex">
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink asChild>
-                    <Link href="#" prefetch={false}>
-                      Dashboard
-                    </Link>
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Overview </BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/auth/dashboard" prefetch={false}>
+                Perfil
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Processos </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3 sm:mb-64">
 
         <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
