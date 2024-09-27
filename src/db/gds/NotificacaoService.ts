44872f71c3f6497ca7ea104a797d21f7329409d1
Mateus Cassuque <mateusabril4@gmail.com>
2024-09-27 20:31:39 +0100

    Notificacao🐱‍💻

--- src/db/gds/NotificacaoService.ts
+++ src/db/gds/NotificacaoService.ts
@@ -1,10 +1,23 @@
 import { INotificacao } from "@/types";
+import prisma from "../prisma.index";
 
-
-export async function NotificacaoUpdate(estado: boolean):Promise<INotificacao | Error> {
+export async function NotificacaoUpdate(notificacaoId: string, estado: boolean): Promise<INotificacao | Error> {
   try {
+    const notificacao = await prisma.notificacao.update({
+      where: {
+        id: notificacaoId
+      },
+      data: {
+        visto: estado
+      }
+    })
+
+    if (!(notificacao instanceof Error)) {
+      return notificacao
+    }
+
     return new Error('Erro ao registrar Pedido, Consulta o fornecedor de Rede.')
   } catch (error) {
-    return new Error((error as {message:string}).message || 'Erro ao Registrar.')
+    return new Error((error as { message: string }).message || 'Erro ao Registrar.')
   }
 }
