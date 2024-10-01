d27f07d3c1c630fddf7250f6bfcff8d59590b5be
Mateus Cassuque <mateusabril4@gmail.com>
2024-10-01 17:14:41 +0100

    Not Childre

--- src/app/_components/layouts/dashboard/aside.tsx
+++ src/app/_components/layouts/dashboard/aside.tsx
@@ -61,30 +61,26 @@ export const AsideBar: React.FC<IAsideProps> = ({ notificacoes }) => {
       <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
         <TooltipProvider>
 
-
-
-          <Tooltip>
-            <TooltipTrigger>
-
-              <AlertDialog open={abrir} onOpenChange={setAbrir}>
-                <AlertDialogTrigger>
-                  <Link href={'#'} className={notificacoes?.length ? "grid grid-cols-2" : "grid grid-cols-1"} onClick={(e) => {
-                    e.preventDefault()
-                    setAbrir(!abrir)
-                  }}>
-                    <Bell className="h-5 w-5" />
-                    {(notificacoes && notificacoes.length > 0) && (
-                      <Badge className="ml-auto flex h-6 w-4 shrink-0 items-center justify-center rounded-full bg-primary">{notificacoes.length ? notificacoes.length : ''}</Badge>
-                    )}
-                  </Link>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>Notificações</AlertDialogTitle>
-                    <div className="w-full flex items-center justify-start">
-                      {notificacoes?.map(not => (
-                        <>
-                          {/* <Card className="w-[350px]">
+          <AlertDialog open={abrir} onOpenChange={setAbrir}>
+            <AlertDialogTrigger>
+              <Link href={'#'} className={notificacoes?.length ? "grid grid-cols-2" : "grid grid-cols-1"} onClick={(e) => {
+                e.preventDefault()
+                setAbrir(!abrir)
+              }}>
+                <Bell className="h-5 w-5" />
+                {(notificacoes && notificacoes.length > 0) && (
+                  <Badge className="ml-auto flex h-6 w-4 shrink-0 items-center justify-center rounded-full bg-primary">{notificacoes.length ? notificacoes.length : ''}</Badge>
+                )}
+              </Link>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Notificações</AlertDialogTitle>
+              </AlertDialogHeader>
+              <div className="w-full flex items-center justify-start">
+                {notificacoes?.map(not => (
+                  <>
+                    {/* <Card className="w-[350px]">
                             <CardHeader className="flex flex-row items-center gap-4 space-y-0">
                               <Avatar>
                                 <AvatarImage src="/placeholder.svg?height=40&width=40" alt="User avatar" />
@@ -106,46 +102,45 @@ export const AsideBar: React.FC<IAsideProps> = ({ notificacoes }) => {
                             </CardFooter>
                           </Card> */}
 
-                          <div className="flex items-center space-x-4 rounded-full p-1 shadow-md bg-black/5">
-                            <Avatar className="h-12 w-12">
-                              {/* <AvatarImage src="/placeholder.svg?height=48&width=48" alt="User avatar" /> */}
-                              {not.tipo == 'contacto' ? (
-                                <MessageCircleWarning className="w-10 h-10 m-2" />
-                              ) : null}
+                    <div className="flex items-center space-x-4 rounded-full p-1 shadow-md bg-black/5">
+                      <Avatar className="h-12 w-12">
+                        {/* <AvatarImage src="/placeholder.svg?height=48&width=48" alt="User avatar" /> */}
+                        {not.tipo == 'contacto' ? (
+                          <MessageCircleWarning className="w-10 h-10 m-2" />
+                        ) : null}
 
-                            </Avatar>
-                            <div className="flex-grow">
-                              <p className="text-sm font-medium">{'Notificação'}</p>
-                              <p className="text-xs text-muted-foreground">{not.mensagem}</p>
-                            </div>
-                            <div className="flex space-x-2">
-                              <Button size="icon" variant="ghost">
-                                <Bell className="h-4 w-4 text-primary" />
-                                <span className="sr-only">Vier notificação</span>
-                              </Button>
-                              <Button size="sm" variant="ghost" className="">
-                                <Eye className="h-4 w-4 text-primary" />
-                                <span className="sr-only">Marcar como lida</span>
-                              </Button>
-                            </div>
-                          </div>
-                        </>
-                      ))}
+                      </Avatar>
+                      <div className="flex-grow">
+                        <p className="text-sm font-medium">{'Notificação'}</p>
+                        <p className="text-xs text-muted-foreground">{not.mensagem}</p>
+                      </div>
+                      <div className="flex space-x-2">
+                        <Button size="icon" variant="ghost">
+                          <Bell className="h-4 w-4 text-primary" />
+                          <span className="sr-only">Vier notificação</span>
+                        </Button>
+                        <Button size="sm" variant="ghost" className="">
+                          <Eye className="h-4 w-4 text-primary" />
+                          <span className="sr-only">Marcar como lida</span>
+                        </Button>
+                      </div>
                     </div>
-                  </AlertDialogHeader>
-                  <AlertDialogCancel className="my-4">Fechar</AlertDialogCancel>
-                </AlertDialogContent>
+                  </>
+                ))}
+              </div>
+              <AlertDialogCancel className="my-4">Fechar</AlertDialogCancel>
+            </AlertDialogContent>
 
-                <AlertDialogFooter>
-                </AlertDialogFooter>
-              </AlertDialog>
+            <AlertDialogFooter>
+            </AlertDialogFooter>
+          </AlertDialog>
 
-
-            </TooltipTrigger>
+          <Tooltip>
+            {/* <TooltipTrigger>Notificações</TooltipTrigger> */}
             <TooltipContent>Notificações</TooltipContent>
           </Tooltip>
 
-          {subListItem.map((link, index) => ( 
+          {subListItem.map((link, index) => (
             <Tooltip key={index}>
               <TooltipTrigger asChild>
                 <Link
